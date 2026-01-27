9dc16e3aa584d4ab3bdd855e4fa492eac9627ca6
Aniket <ranaaniket226@gmail.com>
2026-01-27 13:41:49 +0530

    Added userAgentData  to get device data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
 import SearchPage from './screens/SearchPage'
 
 function App() {
+  console.log("navigator?.userAgentData", navigator?.userAgentData)
+  if (navigator?.userAgentData) {
+    const uaData = navigator?.userAgentData;
+  
+    uaData?.getHighEntropyValues([
+      'platform',
+      'platformVersion',
+      'model',
+      'fullVersionList'
+    ]).then(data => {
+      console.log("JSON.stringify(data)", JSON.stringify(data))
+      window.alert(JSON.stringify(data))
+    });
+  }
   return (<SearchPage />)
 }
 
